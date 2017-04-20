8f81bd692df68ca663f5f8b953d4c937c780888b
ducistheboss <ducistheboss.gmail.com>
2017-04-20 14:48:34 -0400

    init theme

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://alex:mlab10dt8689@ds161190.mlab.com:61190/hyu'
+    // 'url' : 'mongodb://alex:mlab10dt8689@ds161190.mlab.com:61190/hyu'
+    'url' : 'mongodb://localhost/hyu'
 
 };
