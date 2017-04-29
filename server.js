31a734d39fdf2f9abcb0eaa9f2ecb5bb1d77e5de
ducistheboss <ducistheboss.gmail.com>
2017-04-29 10:21:23 -0400

    init user authentication using passport-local

--- server.js
+++ server.js
@@ -1,121 +1,170 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-// var stormpath = require('express-stormpath');
-
 const app = express();
 const host = process.env.IP || '0.0.0.0';
 const port = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
+const session = require('express-session')
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+// setup connection to database
+const configDB = require('./config/database.js');
+mongoose.Promise = require('bluebird');
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
-// setup connection to database
-const configDB = require('./config/database.js');
-mongoose.connect(configDB.url); // connect to our database
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// required for passport
+app.use(session({ secret: 'thisisasecretsession' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 const Phonebanks = require('./models/phonebank.js');
 const Campaigns = require('./models/campaign.js');
 
-// Middleware
-
-	// load all phonebank
-	function loadPhonebanks(req, res, next) {
-
-		Phonebanks.find(function(err, phonebank) {
-	      if(!err) {
-	  			res.locals.phonebank = phonebank;
-	  		}
-	  		else {
-	  			console.log('Error loading phonebanks.');
-	  			res.redirect('/');
-	  		}
-	  		next();
-		  }
-	  );
-	}
-
-	// load all campaigns
-	function loadCampaigns(req, res, next) {
-
-		Campaigns.find({zipcode: req.params.zipcode}, function(err, campaign) {
-				if(!err) {
-					res.locals.campaign = campaign;
-				}
-				else {
-					console.log('Error loading campaigns.');
-					res.redirect('/');
-				}
-				next();
-			}
-		);
-	}
-
+// route
 app.get('/', function(req, res) {
   res.render('home');
 });
 
-app.get('/admin', function(req, res) {
-  res.render('admin/admin');
-});
-
-	// phonebank
-	app.get('/phonebank', loadPhonebanks,function(req, res) {
-	  res.render('phonebank/list');
-	});
-
-	app.post('/phonebank/add', function(req, res) {
-
-	  const newPhoneBank = new Phonebanks();
-
-	  newPhoneBank.candidate = req.body.candidate;
-		newPhoneBank.location = req.body.location;
-	  newPhoneBank.office = req.body.office;
-	  newPhoneBank.party = req.body.party;
-	  newPhoneBank.link = req.body.link;
-	  newPhoneBank.callDate = req.body.callDate;
-
-	  newPhoneBank.save(function(err, phonebank){
-
-	    if(phonebank && !err){
-	      res.redirect('/phonebank');
-	      return;
-	  	}
-	    const errors = "Error adding the phonebank";
-
-	  });
-	});
-
-	// donate campaign
-	app.get('/campaign/:zipcode', loadCampaigns,function(req, res) {
-		res.render('campaign/list');
-	});
-
-	app.post('/campaign/add', function(req, res) {
-		const newCampaign = new Campaigns();
-
-		newCampaign.zipcode = req.body.zipcode;
-		newCampaign.candidate = req.body.candidate;
-		newCampaign.office = req.body.office;
-		newCampaign.party = req.body.party;
-		newCampaign.electionDate = req.body.electionDate;
-
-		newCampaign.save(function(err, campaign){
-
-			if(campaign && !err){
-				res.redirect('/campaign/'+req.body.zipcode);
-				return;
-			}
-			const errors = "Error adding the campaign";
-
+	// user authentication
+		// login
+		app.get('/login', function(req, res) {
+			res.render('login.ejs', { message: req.flash('loginMessage') });
 		});
-	});
+		app.post('/login', passport.authenticate('local-login', {
+			successRedirect : '/profile',
+			failureRedirect : '/login',
+			failureFlash : true
+		}));
+		// signup
+		app.get('/signup', function(req, res) {
+			res.render('signup.ejs', { message: req.flash('signupMessage') });
+		});
+		app.post('/signup', passport.authenticate('local-signup', {
+			successRedirect : '/profile',
+			failureRedirect : '/signup',
+			failureFlash : true
+		}));
+		// profile
+		app.get('/profile', isLoggedIn, function(req, res) {
+			res.render('profile.ejs', {
+				user : req.user
+			});
+		});
+		// logout
+		app.get('/logout', function(req, res) {
+			req.logout();
+			res.redirect('/');
+		});
+
+	// user route
+		// phonebank
+		app.get('/phonebank', loadPhonebanks,function(req, res) {
+		  res.render('phonebank/list');
+		});
+		app.post('/phonebank/add', function(req, res) {
+
+		  const newPhoneBank = new Phonebanks();
+
+		  newPhoneBank.candidate = req.body.candidate;
+			newPhoneBank.location = req.body.location;
+		  newPhoneBank.office = req.body.office;
+		  newPhoneBank.party = req.body.party;
+		  newPhoneBank.link = req.body.link;
+		  newPhoneBank.callDate = req.body.callDate;
+
+		  newPhoneBank.save(function(err, phonebank){
+
+		    if(phonebank && !err){
+		      res.redirect('/phonebank');
+		      return;
+		  	}
+		    const errors = "Error adding the phonebank";
+
+		  });
+		});
+
+		// donate campaign
+		app.get('/campaign/:zipcode', loadCampaigns,function(req, res) {
+			res.render('campaign/list');
+		});
+		app.post('/campaign/add', function(req, res) {
+			const newCampaign = new Campaigns();
+
+			newCampaign.zipcode = req.body.zipcode;
+			newCampaign.candidate = req.body.candidate;
+			newCampaign.office = req.body.office;
+			newCampaign.party = req.body.party;
+			newCampaign.electionDate = req.body.electionDate;
+
+			newCampaign.save(function(err, campaign){
+
+				if(campaign && !err){
+					res.redirect('/campaign/'+req.body.zipcode);
+					return;
+				}
+				const errors = "Error adding the campaign";
+
+			});
+		});
+
+		// Middleware
+
+			// load all phonebank
+			function loadPhonebanks(req, res, next) {
+
+				Phonebanks.find(function(err, phonebank) {
+			      if(!err) {
+			  			res.locals.phonebank = phonebank;
+			  		}
+			  		else {
+			  			console.log('Error loading phonebanks.');
+			  			res.redirect('/');
+			  		}
+			  		next();
+				  }
+			  );
+			}
+
+			// load all campaigns
+			function loadCampaigns(req, res, next) {
+
+				Campaigns.find({zipcode: req.params.zipcode}, function(err, campaign) {
+						if(!err) {
+							res.locals.campaign = campaign;
+						}
+						else {
+							console.log('Error loading campaigns.');
+							res.redirect('/');
+						}
+						next();
+					}
+				);
+			}
+
+			// route middleware to make sure a user is logged in
+			function isLoggedIn(req, res, next) {
+
+			    // if user is authenticated in the session, carry on
+			    if (req.isAuthenticated())
+			        return next();
+
+			    // if they aren't redirect them to the home page
+			    res.redirect('/');
+			}
 
 // server start
 const server = app.listen(port, host, function () {
